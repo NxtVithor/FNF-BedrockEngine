7bb75fa594a46b2f60457f65d4de38c4f0b8f6fb
Gui-iago <lucariothelast91@gmail.com>
2022-01-21 13:20:59 -0400

    MenuBG thing

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -77,7 +77,7 @@ class MainMenuState extends MusicBeatState
 		add(camFollow);
 		add(camFollowPos);
 
-		bg = new FlxSprite(-80).loadGraphic(Paths.image('menuBGdark'));
+		bg = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
 		bg.scrollFactor.set(0, yScroll);
 		bg.setGraphicSize(Std.int(bg.width * 1.175*scaleRatio ));
 		bg.updateHitbox();
